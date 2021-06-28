bc74e15afc305836d90ecabc4289950fed7ebb0f
Dmitriy Tkach <dmitriy.tka4@gmail.com>
2021-06-28 14:18:24 +0700

    Added some files

--- index.js
+++ index.js
@@ -2,4 +2,19 @@
 
 // test
 
-console.log('test git');
+// console.log('test git');
+
+
+function User() {
+  this.name = 'anonimus';
+  this.say = () =>  {
+    console.log(`${this.name}: kiiiya`);
+  };
+}
+
+
+let o = new User();
+
+o.say();
+
+// console.log(o?.address?.street);
